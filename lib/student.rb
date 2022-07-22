25f5fc77938bea3fa62e214d423153aa4dc54e09
Jamie <bennythedev@gmail.com>
2022-07-21 19:28:23 -0500

    commit tree 4d08d5a22f89f4468ce946828979f083ed21e498

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
 
+    attr_accessor :knowledge
+
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(string)
+        @knowledge.push(string)
+    end
+
+
+    def self.knowledge
+        @knowledge
+    end
 end
